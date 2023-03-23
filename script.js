7cc1829106ba1abec0cd16bfb823f31589ff68e6
Nurali_NM3 <nurdtinovnurali@gmail.com>
2023-03-23 15:34:01 +0600

    calculator

--- script.js
+++ script.js
@@ -33,23 +33,27 @@ buttonEqual.addEventListener('click',() =>{
     let f = title.innerText
     if(f.includes('+') ){
         let a = Number(f.slice(0,f.indexOf('+')))
-        let b = Number(f.slice(f.indexOf('+'),f.length))
-        return title.innerText =  a + b
+        let b = Number(f.slice(f.indexOf('+'),f.lastIndexOf('+')))
+        let v = Number(f.slice(f.lastIndexOf('+'),f.length))
+        return title.innerText =  a + b +v
 
     } else if(f.includes('-') ){
         let g = Number(f.slice(0,f.indexOf('-')))
-        let d = Number(f.slice(f.indexOf('-') + 1,f.length))
-        return title.innerText =  g-d
+        let d = Number(f.slice(f.indexOf('-')+1 ,f.lastIndexOf('-')))
+        let s = Number(f.slice(f.lastIndexOf('-') +1 ,f.length))
+        return title.innerText =  g - d - s
 
     }else if(f.includes('*') ){
         let g = Number(f.slice(0,f.indexOf('*')))
-        let d = Number(f.slice(f.indexOf('*') + 1,f.length))
-        return title.innerText =  g * d
+        let d = Number(f.slice(f.indexOf('*') + 1,f.lastIndexOf('*')))
+        let s = Number(f.slice(f.lastIndexOf('*') +1 ,f.length))
+        return title.innerText =  g * d * s
 
     }else if(f.includes('/') ){
         let g = Number(f.slice(0,f.indexOf('/')))
-        let d = Number(f.slice(f.indexOf('/') + 1,f.length))
-        return title.innerText =  g / d
+        let d = Number(f.slice(f.indexOf('/') + 1,f.lastIndexOf('/')))
+        let s = Number(f.slice(f.lastIndexOf('/') +1 ,f.length))
+        return title.innerText =  g / d / s
 
     }
 
